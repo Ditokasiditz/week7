130ed8d157540d92323c5c4a2a37dc8113805c42
Ditokasiditz <kasidit.samad@gmail.com>
2022-09-22 23:52:56 +0700

    start

--- week7_1.c
+++ week7_1.c
@@ -1,8 +1,24 @@
-//จงเขียนโปรแกรมเพื่อรับข้อความ 1 ข้อความ แล้วให้โปรแกรมแสดงผลลัพธ์ว่ามีพยัญชนะอะไรบ้าง อย่างละกี่ตัว (Level 3)
+//จงเขียนโปรแกรมแสดงผล Q3ของชุดข้อมูลที่ใส่เข้าไป (Level 3)
 
 #include<stdio.h>
-#include<string.h>
+
+
 
 int main(){
+    int number[20],i,v;
+    printf("enter value of number:");
+    scanf("%d",&v);
+    for(i=0;i<v;i++) {scanf("%d",&number[i]);}
+
+    for (i = 0; i < n; ++i){
+        for (j = i + 1; j < n; ++j){
+            if (number[i] > number[j]){
+                a =  number[i];
+                number[i] = number[j];
+                number[j] = a;
+                }
+            }
+        }
+    
 
 }
